d2a52812f993972944d2aed1c200df9a5c0140b4
illanchristoffel <58255411+illanchristoffel@users.noreply.github.com>
2020-05-05 02:23:31 +0100

    Final version
    Final version

---
+++ application/views/formulaire/successInscription.php
@@ -0,0 +1,37 @@
+
+<html>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, shrink-to-fit=no">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<head><title>Inscription réussie</title></head>
+
+<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/style.css">
+<body class="body-mob-tab">
+
+<img src="<?php echo base_url() ?>assets/images/background-connexion.jpg" alt="background_chalet"
+	 class="background-connexion">
+<div class="wrap-form-success-inscr">
+
+	<div class="wrap-contents-reservBIS">
+		<div class="wrap-input-success-inscr">
+
+			<span class="message-success-inscr">Inscription validée !</span>
+
+			<a href="/index.php/formulaire/testconnexion"><input type="button" value="Se connecter"></a>
+
+
+		</div>
+	</div>
+</div>
+<?php echo form_close(); ?>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
